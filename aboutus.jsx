b0aff5619a8c9387db716aa22819e4d446c0c6b7
NIVETHA S <149925944+Nivetha1705@users.noreply.github.com>
2024-12-30 19:38:35 +0530

    Add files via upload

---
+++ aboutus.jsx
@@ -0,0 +1,10 @@
+function Aboutus(){
+    return(
+        <div>
+           <h1>Aboutus:</h1>
+           <h2>online shopping store😀</h2>
+           <h2>Purchase and enjoy🛍️</h2> 
+        </div>
+    )
+}
+export default Aboutus;
---
+++ contactus.jsx
@@ -0,0 +1,8 @@
+function Contactus(){
+    return(
+        <div>
+           <h1>Contactus if any help needed at anytime📲</h1> 
+        </div>
+    )
+}
+export default Contactus;
